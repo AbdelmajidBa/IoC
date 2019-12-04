76daaaf1ba946e2f7d9e5e94e0ba80798b9ecb61
BACO Abdelmajid <46558124+AbdelmajidBa@users.noreply.github.com>
2019-12-04 11:12:15 +0100

    Update DataAccessFactory.cs

--- DependencyInversion/DataAccessFactory.cs
+++ DependencyInversion/DataAccessFactory.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,7 +8,7 @@ namespace DependencyInversion
     {
         public static IDataAccess GetDataAccessObj()
         {
-            return new DataAccess();
+            return new DataAccessLayer();
         }
     }
 }
